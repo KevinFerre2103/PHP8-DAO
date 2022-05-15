450795e4600e51ef73f34c02cfba063a5fe4e7a1
Kevin Ferreira <kevinferre210395@outlook.com>
2022-05-15 03:03:06 -0300

    Atualizado class Usuario

--- class/Usuario.php
+++ class/Usuario.php
@@ -2,90 +2,94 @@
 
 class Usuario {
 
-  private $idusuario;
-  private $deslogin;
-  private $desenha;
-  private $dtcadastro;
+    private $idusuario;
+    private $deslogin;
+    private $dessenha;
+    private $dtcadastro;
 
-  public function getIdusuario(){
+    public function getIdusuario(){
 
-    return $this->$idusuario;
-
-  }
-
-  public function setIdusuario($value){
-
-    $this->idusuario = $value;
-
-  }
-
-  public function getDeslogin(){
-
-    return $this->$deslogin;
-
-  }
-
-  public function setDeslogin($value){
-
-    $this->deslogin = $value;
-
-  }
-
-  public function getDesenha(){
-
-    return $this->$desenha;
-
-  }
-
-  public function setDesenha($value){
-
-    $this->desenha= $value;
-
-  }
-
-  public function getDtcadastro(){
-
-    return $this->$dtcadastro;
-
-  }
-
-  public function setDtcadastro($value){
-
-    $this->dtcadastro= $value;
-
-  }
-
-
-  public function loadById($id){
-
-    $sql = new Sql();
-
-    $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
-      ":ID"=>$id
-    ));
-    if(isset($results) > 0) {
-
-      $row = $results[0];
-
-      $this->setIdusuario($row['idusuario']);
-      $this->setDeslogin($row['deslogin']);
-      $this->setDesenha($row['desenha']);
-      $this->setDtcadastro(new DateTime($row['dtcadastro']));
+        return $this->idusuario;
 
     }
-  }
 
-  public function __toString(){
+    public function setIdusuario($value){
 
-    return json_encode(array(
-      "idusuario"=>$this->getIdusuario(),
-      "deslogin"=>$this->getDeslogin(),
-      "desenha"=>$this->getDesenha(),
-      "dtcadastro"=>$this->getDtcadastro()->fromat("d/m/Y H:i:s")
-    ));
+        $this->idusuario = $value;
 
-  }
+    }
 
+    public function getDeslogin(){
+
+        return $this->deslogin;
+
+    }
+
+    public function setDeslogin($value){
+
+        $this->deslogin = $value;
+
+    }
+
+    public function getDesenha(){
+
+        return $this->desenha;
+
+    }
+
+    public function setDesenha($value){
+
+        $this->desenha = $value;
+
+    }
+
+    public function getDtcadastro(){
+
+        return $this->dtcadastro;
+
+    }
+
+    public function setDtcadastro($value){
+
+        $this->dtcadastro = $value;
+
+    }
+
+    public function loadById($id){
+
+        $sql = new Sql();
+
+        $results = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(
+
+            ":ID"=>$id
+
+        ));
+
+        if (count($results) > 0) {
+
+            $row = $results[0];
+
+            $this->setIdusuario($row['idusuario']);
+            $this->setDeslogin($row['deslogin']);
+            $this->setDesenha($row['desenha']);
+            $this->setDtcadastro(new DateTime($row['dtcadastro']));
+
+        }
+
+    }
+
+    public function __toString(){
+
+        return json_encode(array(
+
+            "idusuario"=>$this->getIdusuario(),
+            "deslogin"=>$this->getDeslogin(),
+            "desenha"=>$this->getDesenha(),
+            "dtcadastro"=>$this->getDtcadastro()->format("d/m/Y H:i:s")
+
+        ));
+
+    }
 
 }
 
